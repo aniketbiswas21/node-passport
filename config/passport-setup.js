11e6cc2a79f2b49844983f5c9d38f83c814f753e
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-09-16 13:08:59 +0530

    added more

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./keys");
+const User = require("../models/user-model");
 
 passport.use(
   new GoogleStrategy(
@@ -12,7 +13,22 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       //passport callback function
-      console.log(profile);
+      // check if user already exists in our db
+      User.findOne({ googleId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          //already have the user
+          console.log("user is" + currentUser);
+        } else {
+          new User({
+            username: profile.displayName,
+            googleId: profile.id,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("new User created" + newUser);
+            });
+        }
+      });
     }
   )
 );
